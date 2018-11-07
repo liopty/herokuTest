383327be4572fa97ba7c4549807c0a3c808cacb0
ROBIN Eliott <liopty@gmail.com>
2018-11-07 17:38:04 +0100

    import projet

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const io = socketIO(server);
+//const io = socketIO(server);
 
 const server = express()
   .use(express.static(path.join(__dirname, 'home')))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  
