324224803b60e964c5124ecc062f5fac57e96551
ROBIN Eliott <liopty@gmail.com>
2018-11-07 17:35:24 +0100

    import projet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const io = socketIO(server);
 
 const server = express()
   .use(express.static(path.join(__dirname, 'home')))
@@ -14,5 +15,13 @@ const server = express()
     socket.on('disconnect', function(){
       //Ecrit dans la console lorsqu'un utilisateur se déconnecte
       console.log("un utilisateur s'est déconnecté");
+    });
+
+    //Lors de l'evenement "chat message", le socket lance la fonction
+    socket.on('chat message', function(msg){
+      //Ecrit dans la console le msg
+      console.log(msg);
+      //Emet le msg de l'evenement "chat message" en broadcast (pas sur)
+      io.emit('chat message', msg);
     });
   });
